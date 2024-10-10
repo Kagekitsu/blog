4762f8debeb1014ac7366d51448a83b980b9a6cc
Braden <baj147@yahoo.com>
2024-10-10 11:52:42 -0400

    moved sounds to public folder and added the sounds to the project

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port};`);
 });
+
